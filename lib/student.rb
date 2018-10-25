69f700e2189ba3b8649f942d426348e268904e39
Harry Bloch <harryfbloch@gmail.com>
2018-10-25 22:48:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,9 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+ def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value) 
+    end
+  end
 end
